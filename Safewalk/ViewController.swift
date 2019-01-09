56fe9d040fb3c11d876260bee838ff2d5065bc55
crosenblatt <rosench13@gmail.com>
2019-01-08 19:44:31 -0500

    Initial Commit

---
+++ Safewalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Safewalk
+//
+//  Created by Christopher Rosenblatt on 1/8/19.
+//  Copyright © 2019 acmsigapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
