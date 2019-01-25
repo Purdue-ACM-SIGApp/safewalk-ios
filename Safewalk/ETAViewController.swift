f3cf259e3ec33e8607f8699aa3c437fa6a9965a1
Jacob White <jwhite@jacobs-macbook-air.local>
2019-01-24 19:16:49 -0500

    Create user wait screen

---
+++ Safewalk/ETAViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ETAViewController.swift
+//  Safewalk
+//
+//  Created by Jacob White on 1/17/19.
+//  Copyright © 2019 acmsigapp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ETAViewController: UIViewController {
+    @IBOutlet weak var eta: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
